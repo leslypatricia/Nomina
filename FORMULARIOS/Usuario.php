5c37b40e0feeab83ae9daa4007050a8874c07188
Proyecto317 <73212815+Proyecto317@users.noreply.github.com>
2020-10-26 12:00:49 -0600

    cinco formularios

--- FORMULARIOS/Usuario.php
+++ FORMULARIOS/Usuario.php
@@ -12,9 +12,8 @@ body{
 	box-sizing:border-box;
 }
 .from{
-	/*width:1000px;
-		padding:40px;*/
-		background:black;
+padding:110px;
+background:black;
 	/*  background: -webkit-linear-gradient(right, #000000, #ffffff, #000000, #ffffff);
   background: -o-linear-gradient(right, #ffffff, #000000, #ffffff, #000000);
   background: -moz-linear-gradient(right,#ffffff, #ffffff,#000000, #000000);
@@ -33,7 +32,7 @@ h1{
 	}
 	
 .form-group{
-	width:750px;
+	width:900px;
 	background:#00ced1;
 	padding:20px;
 	border-radius:4px;
@@ -74,84 +73,30 @@ h1{
 <body>
 <form class="from"  id="form1" action="#" method="POST">
 <center>
-<h1>Formulario General</h1>
-
-
+<h1>Formulario Usuario</h1>
 <div class="form-group">
-<label>Cod_Empleado:</label>
-<input type="text" name="CE" value="" size="5" maxlength="5" />
-<label>Cod_Departamento:</label>
-<select name="CD">
-<option value="0"></option>
-   <option value="1">1</option>
-   <option value="2">2</option>
-   <option value="3">3</option>
-   <option  value="4">4</option>
-</select>
-<br/><br/>
+<label>Codigo Usuario:</label>
+<input type="text" name="CU" value="" size="5" maxlength="5" />
 <left>
-<label>N Identidad:</label>
-<input type="text" name="id" value="" size="15" maxlength="15" />
+<label>Usuario:</label>
+<input type="text" name="usuario" value="" size="15" maxlength="15" />
 </left>
 <br/><br/>
-<label>Primer Nombre:</label>
-<input type="text" name="PN" value="" size="20" maxlength="20"/>
-<label>Segundo Nombre:</label>
-<input type="text" name="SN" value=""/>
-<br/>
-<br/>
-<label>Primer Apellido:</label>
-<input type="text" name="PA" value="" size="20" maxlength="20" />
-<label>Segundo  Apellido:</label>
-<input type="text" name="SA" value="" size="20" maxlength="30"/>
-<br/>
-<br/>
-<label>Fecha Nacimiento:</label>
-<input type="date" name="FN" value="" size="20" maxlength="30" />
-<label>Fecha Ingreso:</label>
-<input type="date" name="FI" value="" size="20" maxlength="30" />
-<br/>
-<br/>
+<label>Password:</label>
+<input type="Password" id="Password" name="CJ" value="" size="20" maxlength="20"/>
 <label>Correo:</label>
-<input type="text" name="correo" value="" size="20" maxlength="30" />
-<label>Direccion:</label>
-<input type="text" name="Dire" value="" size="20" maxlength="30" />
-<label>Telefono:</label>
-<input type="text" name="tele" value="" size="20" maxlength="30" />
-<br/>
-<br/>
-<label>Sexo:</label>
- <input type="radio" name="sexo"value="mujer" id="mujer">F</>
-   <input type="radio"  name="sexo" value="hombre" id="hombre">M</>
+<input type="text"  name="CJ" value="" size="20" maxlength="20"/>
 
-<label>Nacionalidad:</label>
-<select name="Nac">
-   <option value="Hondureña">Hondureña</option>
-   <option value="Extrajero">Extrajero</option>
-</select>
-<br/>
-<br/>
-<label>Sueldo Base:</label>
-<input type="text" name="SB" value="" size="15" maxlength="15" />
-<label>Cuenta Bancaria</label>
-<input type="text" name="CB" value="" size="20" maxlength="30" />
 <br/><br/>
-<label>Fecha Devolucion:</label>
-<input type="date" name="FD" value="" size="20" maxlength="30"  />
-<label>Codigo Forma Pago:</label>
-<select name="FP">
-<option value="0"></option>
-   <option value="1">1</option>
-   <option value="2">2</option>
-   <option value="3">3</option>
-   <option  value="4">4</option>
-</select>
+<label>Codigo Rol:</label>
+<input type="text"  name="CR" value="" size="20" maxlength="20"/>
 
-<br/>
-<br/>
+<label>Codigo Empleado:</label>
+<input type="text" name="CE" value="" size="20" maxlength="20"/>
+<br/><br/>
 <center>
-<a href="bd/Menu_Admin.php" class="btn btn-default">Regresar</a>
-<a onClick="Windows.location='Actualizar.php'" class="btn btn-default">listar</a>
+<a href="LOG/Menu_Admin.php" class="btn btn-default">Regresar</a>
+
 <span class="icon-floppy-disk"></span>
 <input type="submit" name="btn-guardar" value="Guardar" class="btn-btn-success" >
 
@@ -160,16 +105,14 @@ h1{
 
 <span class="icon-bin"></span>
 <input type="submit" name="btn-eliminar" value="Eliminar" class="btn-btn-danger">
-<br/>
-<br/>
 
-
-<label>Cod_Empleado:</label>
-<input type="text" name="CE" value="" size="5" maxlength="5" />
-<span class="icon-search">
+<span class="icon-search"></span>
 <input type="submit" name="btn-buscar" value="Buscar" class="btn-btn-primary"></span>
-<br/>
-<br/>
+<br/><br/>
 </center>
+
 </div>
-</center></form></body></html>
+</center>
+</form>
+</body>
+</html>
